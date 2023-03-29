22e48c4e40ca8c4fcf436368cab1481f37129618
Sudhakarsiva18 <108916787+Sudhakarsiva18@users.noreply.github.com>
2023-03-29 09:36:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import World from './day1/World';
+
+
+function App() {
+  return (
+    <div className="App">
+      This Our Project
+<World/>
+    </div>
+  );
+}
+
+export default App;
